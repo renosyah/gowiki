6a848796622a0ba103464582a586855738ab83f3
renosyah <syahputrareno975@gmail.com>
2016-10-11 11:11:43 +0700

    first commit

---
+++ gowiki5.go
@@ -0,0 +1,52 @@
+package main
+     	
+  import (
+
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+    	
+    	type Page struct {
+   	Title string
+    	Body  []byte
+    	}
+    
+    	func (p *Page) save() error {
+    		filename := p.Title + "test.txt"
+    		return ioutil.WriteFile(filename, p.Body, 0600)
+    	}
+    	
+    	func loadPage(title string) (*Page, error) {
+    		filename := title + "test.txt"
+    		body, err := ioutil.ReadFile(filename)
+    		if err != nil {
+    			return nil, err
+    		}
+    		return &Page{Title: title, Body: body}, nil
+    	}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w, p)
+}
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    t, _ := template.ParseFiles("view.html")
+    t.Execute(w, p)
+}
+    	
+    	func main() {
+    	http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+//    http.HandleFunc("/save/", saveHandler)
+    		http.ListenAndServe(":7070", nil)
+    	}
